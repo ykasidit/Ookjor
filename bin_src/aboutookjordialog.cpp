1b1456d46165b9b6c547497a73e72f5b26cb4106
kasidit <kasidit@kasidit-desktop.(none)>
2010-01-03 09:07:36 +0700

    now about dlg loads from README

--- bin_src/aboutookjordialog.cpp
+++ bin_src/aboutookjordialog.cpp
@@ -1,11 +1,27 @@
 #include "aboutookjordialog.h"
 #include "ui_aboutookjordialog.h"
+#include <QFileInfo>
 
 aboutookjordialog::aboutookjordialog(QWidget *parent) :
     QDialog(parent),
     m_ui(new Ui::aboutookjordialog)
 {
     m_ui->setupUi(this);
+
+    QString readmepath;
+    readmepath += QFileInfo( QCoreApplication::argv()[0] ).absolutePath();
+    readmepath += "/../README";
+
+    QFile f( readmepath );
+    if(f.open(QIODevice::ReadOnly))
+    {
+        QString s(f.readAll());
+        m_ui->textBrowser->setFrameStyle(QFrame::Plain);
+        m_ui->textBrowser->setText(s);
+        f.close();
+    }
+    else
+        qDebug("open README failed");
 }
 
 aboutookjordialog::~aboutookjordialog()
